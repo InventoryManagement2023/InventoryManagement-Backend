c6a0f3051d712febe890c00ad8dacf6dd2689d06
Lukas Proksch <2120859001@fh-burgenland.at>
2023-04-24 20:24:21 +0200

    [IM-11] use in memory authentication provider in dev environment instead of ldap; User: Super Admin Password: password

--- userControlWebservice/src/main/java/net/inventorymanagement/usercontrolwebservice/configuration/UserControlConfiguration.java
+++ userControlWebservice/src/main/java/net/inventorymanagement/usercontrolwebservice/configuration/UserControlConfiguration.java
@@ -1,11 +1,16 @@
 package net.inventorymanagement.usercontrolwebservice.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.ldap.authentication.ad.ActiveDirectoryLdapAuthenticationProvider;
 
 /**
@@ -16,27 +21,53 @@ import org.springframework.security.ldap.authentication.ad.ActiveDirectoryLdapAu
 @EnableWebSecurity
 public class UserControlConfiguration extends WebSecurityConfigurerAdapter {
 
-    // ldap-instance
+    @Autowired
+    private Environment environment;
+
+    // ldap-instance in production, otherwise an in-memory authentication
     @Override
-    protected void configure(AuthenticationManagerBuilder auth) {
-        ActiveDirectoryLdapAuthenticationProvider adProvider =
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        if (isProduction()) {
+            ActiveDirectoryLdapAuthenticationProvider adProvider =
                 new ActiveDirectoryLdapAuthenticationProvider("YOUR.DOMAIN.net", "ldap://IP.OF.YOUR.LDAP:389");
-        adProvider.setConvertSubErrorCodesToExceptions(true);
-        adProvider.setUseAuthenticationRequestCredentials(true);
-        auth.authenticationProvider(adProvider);
+            adProvider.setConvertSubErrorCodesToExceptions(true);
+            adProvider.setUseAuthenticationRequestCredentials(true);
+            auth.authenticationProvider(adProvider);
+        } else {
+            auth
+                .inMemoryAuthentication()
+                .withUser("Super Admin")
+                .password(passwordEncoder().encode("password"))
+                .authorities("ROLE_USER");
+        }
+
     }
 
     // authentication-interface
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .cors()
-                .and()
-                .authorizeRequests()
-                .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .httpBasic();
+            .cors()
+            .and()
+            .authorizeRequests()
+            .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+            .anyRequest().authenticated()
+            .and()
+            .httpBasic();
     }
 
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    private boolean isProduction() {
+        String[] environments = this.environment.getActiveProfiles();
+        for (String env : environments){
+            if (env.equalsIgnoreCase("prod")) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
